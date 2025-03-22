e54b0f408b8e4f24ac15fa64f7a9c4b953fc4c22
luciusvie2712 <vinh15062703@gmail.com>
2025-03-23 01:03:53 +0700

    tiep ne

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 // dang ky tai khoan
 const register = async (req, res) => {
     try {
-        const { username, email, password } = req.body
+        const { username, email, password, role } = req.body
 
         // kiem tra email da ton tai chua 
         const existingUser = await User.findOne({ email })
@@ -14,7 +14,7 @@ const register = async (req, res) => {
 
         // ma hoa mat khau
         const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = new User({username, email, password: hashedPassword})
+        const newUser = new User({username, email, password: hashedPassword, role: role || 'user' }) // khong the tu dang ky thanh admin
         await newUser.save()
         res.json({ message: 'Register is success!'})
     } catch (error) {
@@ -30,8 +30,11 @@ const login = async (req, res) => {
         const user = await User.findOne({ email })
         if (!user) 
             return res.status(400).json({ msg: 'Email is not available!'})
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch)
+            return res.status(400).json({ msg: 'Password is not correct!'})
         
-        // taoj JWT
+        // tao JWT
         const token = jwt.sign({ userId: user._id, role: user.role }, process.env.SECRET_KEY, { expiresIn: '1h' })
         res.json({ message: 'Login is success!', token, user: {id: user._id, username: user.username, email: user.email, role: user.role} })
 
